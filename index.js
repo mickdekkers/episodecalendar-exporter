ca0fd4cea246cdbe5810065e65c73040eb5101e5
Mick Dekkers <mick.dekkers@tradecast.eu>
2019-02-11 20:10:01 +0100

    refactor: rewrite to handle login purely in node
    Also get rid of lowdb for now

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const puppeteer = require('puppeteer')
-
 const fs = require('fs')
 const fetch = require('node-fetch')
 const filesize = require('filesize')
 const progressStream = require('progress-stream')
-
-const low = require('lowdb')
-const FileAsync = require('lowdb/adapters/FileAsync')
+const cheerio = require('cheerio')
+const { parse, splitCookiesString } = require('set-cookie-parser')
+const serializeForm = require('form-urlencoded').default
 
 const debug = require('debug')
 const debugEpcal = debug('epcal:main')
-const debugLogin = debug('epcal:login')
+const debugAuth = debug('epcal:login')
 const debugDown = debug('epcal:download')
+
+const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 
 /**
  * Create a Promise that resolves when a stream ends
@@ -26,87 +27,124 @@ const streamCompletion = stream =>
     stream.on('error', reject)
   })
 
-/**
- * Simple helper function that converts a cookie object to a name=value string
- */
-const stringifyCookie = ({ name, value }) => `${name}=${value}`
+const parseSetCookie = headers => {
+  const SESSION_TOKEN_KEY = '_episodecalendar_session'
+  const setCookieHeader = headers.get('Set-Cookie')
 
-/**
- * Log in to episodecalendar.com to retrieve a session cookie
- * @param {*} db - The db to write the session cookie to
- * @param {*} user - The user to log in as
- * @returns {Promise<{}>} - The session cookie
- */
-const login = async (db, user) => {
-  // Launch browser
-  debugLogin('launching browser')
-  const browser = await puppeteer.launch({ headless: true })
+  if (!setCookieHeader) {
+    throw new Error('Unable to get session: no Set-Cookie header present')
+  }
 
-  // Open new page
-  debugLogin('opening new page')
-  const page = await browser.newPage()
+  const parsedHeader = parse(splitCookiesString(setCookieHeader), {
+    map: true,
+    decodeValues: false,
+  })
 
-  // Go to the login page
-  debugLogin('navigating to login page')
-  await page.goto('https://episodecalendar.com/en/account/sign_in')
+  if (!parsedHeader[SESSION_TOKEN_KEY]) {
+    throw new Error(
+      `Unable to get session: Set-Cookie header has no ${SESSION_TOKEN_KEY} value`,
+    )
+  }
 
-  // Enter email
-  debugLogin('entering email address')
-  await page.focus('input#user_email')
-  await page.type(user.email)
+  const session = parsedHeader[SESSION_TOKEN_KEY].value
 
-  // Enter password
-  debugLogin('entering password')
-  await page.focus('input#user_password')
-  await page.type(user.password)
+  return `${SESSION_TOKEN_KEY}=${session}`
+}
 
-  // Click remember
-  debugLogin('clicking remember me')
-  await page.click('input#user_remember_me')
+const getSession = async () => {
+  const res = await fetch('https://episodecalendar.com/en/account/sign_in', {
+    headers: {
+      Accept: 'text/html',
+      'User-Agent': USER_AGENT,
+    },
+  })
 
-  // Click submit
-  debugLogin('submitting login form')
-  await page.click('input[type="submit"]')
+  if (!res.ok) {
+    throw new Error(
+      `getSession call failed: unexpected status code ${res.status}`,
+    )
+  }
 
-  // Wait for next page to load
-  debugLogin('waiting for next page load')
-  await page.waitForNavigation()
+  const sessionCookie = parseSetCookie(res.headers)
 
-  // Read session cookie
-  debugLogin('reading session cookie')
-  const sessionCookie = (await page.cookies()).find(
-    x => x.name === '_episodecalendar_session',
-  )
+  const authToken = cheerio
+    .load(await res.text())(
+      'form[action="/users/sign_in"] input[name=authenticity_token]',
+    )
+    .attr('value')
 
-  // Close browser
-  debugLogin('closing browser')
-  await browser.close()
+  if (typeof authToken !== 'string' || !authToken.length) {
+    throw new Error('unable to extract auth token from getSession response')
+  }
 
-  debugLogin('writing session cookie to db')
-  await db.set('sessionCookie', sessionCookie).write()
+  return {
+    authToken,
+    sessionCookie,
+  }
+}
 
-  return sessionCookie
+const login = async (authToken, sessionCookie, user) => {
+  const res = await fetch('https://episodecalendar.com/users/sign_in', {
+    // Don't follow redirects; the headers we need are in the first response
+    redirect: 'manual',
+    method: 'POST',
+    headers: {
+      Accept: 'text/html',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Cookie: `${sessionCookie}; omniauth_remember_me=true`,
+      'User-Agent': USER_AGENT,
+    },
+    body: serializeForm({
+      authenticity_token: authToken,
+      'user[email]': user.email,
+      'user[password]': user.password,
+      'user[remember_me]': '1',
+    }),
+  })
+
+  if (!(res.status >= 200 && res.status < 400)) {
+    throw new Error(`login call failed: unexpected status code ${res.status}`)
+  }
+
+  // TODO: do we want/need to return the `remember_user_token` cookie as well?
+  return parseSetCookie(res.headers)
 }
 
 /**
- * Download the user data json given a session cookie for authentication
+ * Log in to episodecalendar.com to retrieve a session cookie
+ */
+const authenticate = async user => {
+  debugAuth('starting authentication process')
+
+  const { authToken, sessionCookie } = await getSession()
+  debugAuth('session initialized')
+
+  const _sessionCookie = login(authToken, sessionCookie, user)
+  debugAuth('logged in')
+
+  return _sessionCookie
+}
+
+/**
+ * Download the user data csv given a session cookie for authentication
  */
 const downloadUserData = async sessionCookie => {
   debugDown('requesting data')
-  const res = await fetch('https://episodecalendar.com/en/export_data/json', {
+  const res = await fetch('https://episodecalendar.com/en/export_data/csv', {
     headers: {
-      Cookie: stringifyCookie(sessionCookie),
+      Accept: 'text/csv',
+      Cookie: sessionCookie,
     },
   })
 
   // TODO: proper error handling
-  if (res.status !== 200) {
-    throw new Error(`Unexpected response code ${res.status}`)
+  if (!res.ok) {
+    throw new Error(`download failed: unexpected status code ${res.status}`)
   }
 
   debugDown(`connection established (${res.status}), downloading data`)
 
-  const file = fs.createWriteStream('user-data.json')
+  const file = fs.createWriteStream('user-data.csv')
 
   const progress = progressStream({
     length: res.size,
@@ -121,7 +159,6 @@ const downloadUserData = async sessionCookie => {
     )
   })
 
-  // TODO: streamed json formatting
   await streamCompletion(res.body.pipe(progress).pipe(file))
 
   debugDown('download complete')
@@ -131,6 +168,7 @@ const downloadUserData = async sessionCookie => {
  * The main function
  */
 const main = async () => {
+  debugEpcal('starting')
   debugEpcal('reading user from env')
   const user = {
     email: process.env.EPCAL_EMAIL,
@@ -138,24 +176,13 @@ const main = async () => {
     password: new Buffer(process.env.EPCAL_PASS, 'base64').toString('utf8'),
   }
 
-  debugEpcal('connecting to db')
-  const db = await low(new FileAsync('db.json'))
-  await db.defaults({ sessionCookie: null }).write()
-  debugEpcal('connected to db')
-
-  let sessionCookie = await db.get('sessionCookie').value()
-  if (sessionCookie == null) {
-    debugEpcal('no session cookie, logging in')
-    sessionCookie = await login(db, user)
-  }
-
+  debugEpcal('no session cookie, logging in')
+  const sessionCookie = await authenticate(user)
   debugEpcal('session cookie retrieved')
+
   debugEpcal('downloading user data')
-
   await downloadUserData(sessionCookie)
-
   debugEpcal('done')
 }
 
-debugEpcal('starting')
 main()
